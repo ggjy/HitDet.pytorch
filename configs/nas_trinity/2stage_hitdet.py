0889383a8015e5d30fea903f26c98bf6b1aea029
Nevermore <348797244@qq.com>
2020-07-21 03:45:25 +0800

    Update 2stage_hitdet.py
    Fix typo.

--- configs/nas_trinity/2stage_hitdet.py
+++ configs/nas_trinity/2stage_hitdet.py
@@ -123,7 +123,7 @@ train_pipeline = [
     dict(type='LoadAnnotations', with_bbox=True),
     dict(
         type='Resize',
-        img_scale=(1333, 400),
+        img_scale=(1333, 800),
         keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
@@ -135,7 +135,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 400),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
